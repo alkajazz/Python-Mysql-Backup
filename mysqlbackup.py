2758ebe74ce29c2927a8cf456bb61d4dcea670e9
Alkajazz <nathanbooth@gmail.com>
2015-08-05 12:12:35 -0500

    Added dir checking and conf creation

--- mysqlbackup.py
+++ mysqlbackup.py
@@ -12,11 +12,27 @@ mkfile = '/etc/fart/test'
 #Check to see if dir exists if not create it#
 #############################################
 
-if not os.path.exists(pathdir): os.makedirs(pathdir)
+def checkdir(path):
+    if not os.path.exists(pathdir): os.makedirs(pathdir)
 
 ##################################################
 #Check to see if Conf is created if not create it#
 ##################################################
 
-if not os.path.exists(mkfile):
-    file(mkfile, 'w').close()
+
+def createfile(mkfile):
+    if os.path.exists(mkfile) == True:
+        print('file exists')
+    else:
+        if not (os.path.isfile(mkfile)):
+            mkuser = input('What is your database user name? ')
+            mkpass = input('What is your password? ')
+            mkhost = input('what is your host? ')
+            f = open(mkfile, 'w')
+            f.write('#This is the config file for the script\n \n[Login]\n \nUsername = %s\n \nPassword= %s\n \nHost = %s' % (mkuser, mkpass, mkhost))
+            f.close()
+
+
+
+checkdir(path)
+createfile(mkfile)
